224579f5bd03b0c7d3cc476e991f54185354fe46
Shivam Aggarwal <shivamaggarwal1821@gmail.com>
2024-05-25 12:56:20 +0530

    Updated the profile dropdown section and styled it

--- src/components/core/common/Navbar.jsx
+++ src/components/core/common/Navbar.jsx
@@ -129,7 +129,7 @@ const Navbar = () => {
                 }
                 {
                     token!==null && (
-                        <ProfileDropDown/>
+                        <ProfileDropDown />
                     )
                 }
             </div>
--- src/components/core/common/ProfileDropDown.jsx
+++ src/components/core/common/ProfileDropDown.jsx
@@ -1,46 +1,61 @@
-import React, { useRef, useState } from 'react';
-import UseOutsideClick from '../hooks/useOutsideClick';
-import trash from '../../../assets/Images/Instructor.png'
-import { logout } from '../../../services/operations/authAPI';
-import Dropdown from './Dropdown';
+import { useRef, useState } from "react";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { VscDashboard, VscSignOut } from "react-icons/vsc";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
-import { useDispatch,useSelector } from 'react-redux'
-import { Link, useNavigate} from 'react-router-dom'
+import useOnClickOutside from "../../../hooks/useOnClickOutside";
+import { logout } from "../../../services/operations/authAPI";
 
-const ProfileDropDown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const navigate=useNavigate();
-  const dispatch=useDispatch();
-  const {user}=useSelector((state)=>state.profile);
-  const profileImg=user?user.image:trash;
-  const menuRef=useRef();
-  const imgRef=useRef();
+export default function ProfileDropdown() {
+    const { user } = useSelector((state) => state.profile);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+    const ref = useRef(null);
 
+    useOnClickOutside(ref, () => setOpen(false));
 
-  const onClose = () => {
-    setIsOpen(false);
-  };
+    if (!user) return null;
 
-  function handleLogout(){
-    dispatch(logout(navigate));
-  }
-
-
-  window.addEventListener("click",(e)=>{
-    console.log(e.target===menuRef.current);
-  })
-
-
-  return (
-    <div className='relative'>
-      <button onClick={() => setIsOpen(!isOpen)} className='relative'>
-        <img src={profileImg} ref={imgRef} width={30} height={30} className='rounded-full'/>
-      </button>
-      <div className='absolute top-50 right-0 ' ref={menuRef}>
-        {isOpen && <Dropdown />}
-      </div>
-    </div>
-  );
-};
-
-export default ProfileDropDown;
+    return (
+        <button className="relative" onClick={() => setOpen(true)}>
+            <div className="flex items-center gap-x-1">
+                <img
+                    src={user?.image}
+                    alt={`profile-${user?.firstName}`}
+                    className="aspect-square w-[35px] rounded-full object-cover"
+                    // width={70}
+                />
+                {/* <AiOutlineCaretDown className="text-sm text-richblack-100" /> */}
+            </div>
+            {open && (
+                <div
+                    onClick={(e) => e.stopPropagation()}
+                    className="absolute top-[118%] right-0 z-[1000] divide-y-[1px] divide-richblack-700 overflow-hidden rounded-md border-[1px] border-richblack-700 bg-richblack-800"
+                    ref={ref}
+                >
+                    <Link
+                        to="/dashboard/my-profile"
+                        onClick={() => setOpen(false)}
+                    >
+                        <div className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25">
+                            <VscDashboard className="text-lg" />
+                            Dashboard
+                        </div>
+                    </Link>
+                    <div
+                        onClick={() => {
+                            dispatch(logout(navigate));
+                            setOpen(false);
+                        }}
+                        className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25"
+                    >
+                        <VscSignOut className="text-lg" />
+                        Logout
+                    </div>
+                </div>
+            )}
+        </button>
+    );
+}
